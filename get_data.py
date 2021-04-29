45a91b22501c7872652ab403d3f3d1aa9ba6d3dc
Samuel Albrecht <elsamuko@gmail.com>
2021-04-29 17:48:58 +0200

    fix: get all data from website + interpolate last column

--- get_data.py
+++ get_data.py
@@ -3,10 +3,12 @@
 # sudo apt install firefox-geckodriver
 
 import time
+import datetime
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 import plotter
+import writer
 
 
 def enter_by_selector(driver, selector: str, text: str) -> None:
@@ -73,23 +75,31 @@ lines = table.split('\n')
 lines.pop(0)
 keys = [k for k in lines[0].split()[1:]]
 
-n_rows = len(keys)
-n_columns = len(lines)
+n_columns = len(keys)
+n_rows = len(lines)
 
 print(f"{n_rows}x{n_columns}")
 
 
 def data_at(c, r):
-    el_table = f"/html/body/form[1]/div[3]/div[1]/div[2]/div/div[2]/div/div/div[5]/div/div/table/tbody/tr[{3+c}]/td[{2+r}]"
+    el_table = f"/html/body/form[1]/div[3]/div[1]/div[2]/div/div[2]/div/div/div[5]/div/div/table/tbody/tr[{3+r}]/td[{2+c}]"
     el_data = driver.find_element_by_xpath(el_table).text
     return float(el_data) if el_data else 0.0
 
 
-data = [[data_at(c, r) for r in range(n_rows-1)] for c in range(n_columns-1)]
+data = [[data_at(c, r) for c in range(n_columns+1)] for r in range(n_rows-1)]
 
 print(keys)
 print(lines)
-print(data)
+# print(data)
+
+# interpolate last column to full week
+_, _, day = datetime.date.today().isocalendar()
+for r in range(len(data)):
+    data[r][-1] = round(data[r][-1] * 7/day, 2)
+
+# write to CSV
+writer.dump_data(data)
 
 # plot data
 plotter.plot(data)
